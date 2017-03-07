f5aa63de09943cf57546900486fea676a75ccc85
Yiyang Shen <yshen@berkeley.edu>
2017-03-06 16:23:27 -0800

    init model

--- docs/models.py
+++ docs/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
-class Docs(models.Model):
-	title = models.TextField()
+class Doc(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
--- docs/views.py
+++ docs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Docs
+from .models import Doc
 
 def index(request):
 	return render(request, 'docs/index.html', {})
